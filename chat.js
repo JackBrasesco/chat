7298bd6bce7ad98d03829afb27e95f9a7c466d33
JackBrasesco <jacbras@nuevaschool.org>
2016-10-24 21:26:49 -0700

    Classadoo autocommit

--- chat.js
+++ chat.js
@@ -1,9 +1,9 @@
 var input = $("#input")
 
 input.keydown(function(e) {
-  if (e == 13) {
+  if (e.keyCode == 13) {
     var message = input.val()
-    addListItem("chat"
+    addListItem("chat", message)
   }
   
 })
