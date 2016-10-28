2e0e1dbba9413b9e53125a900eea876a98566b84
JackBrasesco <jacbras@nuevaschool.org>
2016-10-28 14:59:16 -0700

    Classadoo autocommit

--- chat.js
+++ chat.js
@@ -8,13 +8,13 @@ input.keydown(function(e) {
     var message = input.val()
     var antijosh = message.replace(/</g, "&lt;").replace(/>/g, "&gt;");
     addListItem("saylist", antijosh);
-    input.val("")
+    input.val("");
   }
 })
-console.log("hi")
+
 onNewListItem("saylist", function(r) {
-  chatlist.push(r)
-  var limit = chatlist.slice(chatlist.length - 30, chatlist.length)
-  chat.html(limit.join("<br>"))
+  chatlist.push(r);
+  var limit = chatlist.slice(chatlist.length - 30, chatlist.length);
+  chat.html(limit.join("<br>"));
  })
 
