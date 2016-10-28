8c13f5e785a18630fed08d8c9b4f66b035df9122
JackBrasesco <jacbras@nuevaschool.org>
2016-10-28 09:32:06 -0700

    Classadoo autocommit

--- chat.js
+++ chat.js
@@ -1,9 +1,12 @@
 var input = $("#input")
 
+var chatlist = []
+
 input.keydown(function(e) {
   if (e.keyCode == 13) {
     var message = input.val()
     var antijosh = message.replace(/</g, "&lt;").replace(/>/g, "&gt;");
-    addListItem("saylist", antijosh); 
+    addListItem("saylist", antijosh);
+    
   }
 })
