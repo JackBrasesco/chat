87f3bd5811cb5e47033aa6c911cf687b26ed42df
JackBrasesco <jacbras@nuevaschool.org>
2016-10-29 16:26:07 -0700

    Classadoo autocommit

--- accounts.js
+++ accounts.js
@@ -54,7 +54,6 @@ var password = $("#password");
 //On button press --------------
 
 $("#signbutton").click(function() {
-  console.log("hi")
   var realUser = username.val()
   var realPass = password.val()
   once(realUser, function(password) {
@@ -62,8 +61,13 @@ $("#signbutton").click(function() {
       console.log("It works!");
     }
       else {
-        $("#wrong").css({
+        $("#wrong").css({display: "block"})
       }
   })
 })
 
+//Sign in session -------------------------------------------
+
+var session = {
+  name: 
+}
