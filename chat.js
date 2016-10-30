9679e17c66ee9610047146cf5772c07a93b91fb1
JackBrasesco <jacbras@nuevaschool.org>
2016-10-30 08:47:07 -0700

    Classadoo autocommit

--- chat.js
+++ chat.js
@@ -13,7 +13,7 @@ input.keydown(function(e) {
   if (e.keyCode == 13) {
     var message = input.val();
     var antijosh = message.replace(/</g, "&lt;").replace(/>/g, "&gt;");
-    var useUser = (realUser.bold().fontcolor("blue") + ": ".bold().fontcolor("blue") + antijosh)
+    var useUser = (realUser.bold().fontcolor("blue") + ": ".bold().fontcolor(accountColor) + antijosh)
     addListItem("saylist", useUser);
     input.val("");
   }
