287bf5e462b1c0cdb68078ad612d25e6cd88a34f
JackBrasesco <jacbras@nuevaschool.org>
2016-11-01 10:02:42 -0700

    Classadoo autocommit

--- chat.js
+++ chat.js
@@ -14,15 +14,9 @@ input.keydown(function(e) {
   if (e.keyCode == 13) {
     var message = input.val();
     var antijosh = message.replace(/</g, "&lt;").replace(/>/g, "&gt;");
-    var cleanerUser = (String(jackIsDumb))
-    var ccleanerUser = cleanerUser.replace(/</g, "&lt;").replace(/>/g, "&gt;")
-    if (ccleanerUser.length < 10) {
-      digitChecker = "t"
-    }
-    if (ccleanerUser.length > 9) {
-      digitChecker = "f"
-    }
-    var useUser = (ccleanerUser + ": " + antijosh + userLength + digitChecker)
+    var cleanerUser = (String(jackIsDumb));
+    var ccleanerUser = cleanerUser.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var useUser = (ccleanerUser + ": " + antijosh);
     addListItem("saylist", useUser);
     input.val("");
   }
@@ -31,8 +25,9 @@ input.keydown(function(e) {
 // Display when new things are added _______________________________
 
 onNewListItem("saylist", function(r) {
-  var reclean = (r.replace(/</g, "&lt;").replace(/>/g, "&gt;"))
-  chatlist.push(reclean)
+  var reclean = (r.replace(/</g, "&lt;").replace(/>/g, "&gt;"));
+  chatlist.push(reclean);
+  var workPlease = 
   var limit = chatlist.slice(chatlist.length - 20, chatlist.length);
   chat.html(limit.join("<br>"));
  })
