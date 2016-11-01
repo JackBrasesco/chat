b36d9055e16acc51de4ea8f9059c63d285aed2d2
JackBrasesco <jacbras@nuevaschool.org>
2016-11-01 07:38:00 -0700

    Classadoo autocommit

--- chat.js
+++ chat.js
@@ -15,7 +15,7 @@ input.keydown(function(e) {
     var antijosh = message.replace(/</g, "&lt;").replace(/>/g, "&gt;");
     var cleanerUser = (String(jackIsDumb))
     var ccleanerUser = cleanerUser.replace(/</g, "&lt;").replace(/>/g, "&gt;")
-    var userLength = ccleanerUser.length
+    var userLength = (ccleanerUser.length)
     var useUser = (ccleanerUser + ": " + antijosh + userLength)
     addListItem("saylist", useUser);
     input.val("");
@@ -26,7 +26,7 @@ input.keydown(function(e) {
 
 onNewListItem("saylist", function(r) {
   var reclean = (r.replace(/</g, "&lt;").replace(/>/g, "&gt;"))
-  var stylize = (reclean
+  var userNumber = (reclean.length -1)
   chatlist.push(reclean)
   var limit = chatlist.slice(chatlist.length - 20, chatlist.length);
   chat.html(limit.join("<br>"));
