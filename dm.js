182977c0bc603af9a06c0c2004e7d627812a89a9
JackBrasesco <jacbras@nuevaschool.org>
2016-11-05 14:58:53 -0700

    Classadoo autocommit

--- dm.js
+++ dm.js
@@ -23,8 +23,8 @@ var listOfAllUsers = []
 newChat.keydown(function(e) {
   if (e.keyCode == 13) {
     var startChatWith = newChat.val()
-    once("listOfUsers", function(r) {
-      var cleanDat = (r.replace(/</g, "&lt;").replace(/>/g, "&gt;"));
+    once("listOfUsers", function(m) {
+      var cleanDat = (m.replace(/</g, "&lt;").replace(/>/g, "&gt;"));
       var usernames = (cleanDat.split(",/")[1]);
       console.log(usernames)
     })
