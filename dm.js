c2a11728232917f7ddfe6ceff697acb70821be65
JackBrasesco <jacbras@nuevaschool.org>
2016-11-05 10:51:13 -0700

    Classadoo autocommit

--- dm.js
+++
@@ -1,9 +0,0 @@
-//buttons -----------------------------------
-
-var dmButton = $("#dm");
-
-//Show button? ------------------------------
-
-if (signedin == 0) {
-  dmButton.css({display: "none"})
-}
