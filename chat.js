50a4516654e6baaa651cf4b705efd9d109bac6cf
JackBrasesco <jacbras@nuevaschool.org>
2016-11-06 14:18:54 -0800

    Classadoo autocommit

--- chat.js
+++ chat.js
@@ -15,7 +15,8 @@ var usableTimeHour = getMessageHour.toString()
 var millitaryTimeIsDumb = usableTimeHour.replace(/13/g, "1").replace(/14/g, "2").replace(/15/g, "3").replace(/16/g, "4").replace(/17/g, "5").replace(/18/g, "6").replace(/19/g, "7").replace(/20/g, "8").replace(/21/g, "9").replace(/22/g, "10").replace(/23/g, "11").replace(/24/g, "12")
 var getMessageMinute = new Date().getMinutes()
 var usableTimeMinute = getMessageMinute.toString()
-console.log(usableTimeMinute)
+var timeForMessages = ("This message was sent at " + millitaryTimeIsDumb + ":" + usableTimeMinute)
+console.log(timeForMessages)
 
 // When you press enter ____________________________________________
 
