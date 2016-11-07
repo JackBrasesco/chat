a3ea24aba464959f725fc13d8daa62372e47b7a8
JackBrasesco <jacbras@nuevaschool.org>
2016-11-06 17:31:02 -0800

    Classadoo autocommit

--- chat.js
+++ chat.js
@@ -33,6 +33,9 @@ input.keydown(function(e) {
     var millitaryTimeIsDumb = usableTimeHour.replace(/13/g, "1").replace(/14/g, "2").replace(/15/g, "3").replace(/16/g, "4").replace(/17/g, "5").replace(/18/g, "6").replace(/19/g, "7").replace(/20/g, "8").replace(/21/g, "9").replace(/22/g, "10").replace(/23/g, "11").replace(/24/g, "12")
     var getMessageMinute = new Date().getMinutes()
     var usableTimeMinute = getMessageMinute.toString()
+    if (usableTimeMinute.length == 1) {
+      usableTimeMinute = ("0" + 
+    }
     var timeForMessages = (millitaryTimeIsDumb + ":" + usableTimeMinute)
     addListItem("saylist", ccleanerUser +"\\\," + antijosh + "\\\," + accountColor + "\\\," + timeForMessages + "\\\," + usableTimeHour);
     input.val("");
